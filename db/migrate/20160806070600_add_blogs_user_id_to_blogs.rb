022e7ce5de3608a6603c7ff3910bf8bdf687f534
hiramatch <hiramatch@gmail.com>
2016-08-06 12:00:28 +0000

    divi09

---
+++ db/migrate/20160806070600_add_blogs_user_id_to_blogs.rb
@@ -0,0 +1,5 @@
+class AddBlogsUserIdToBlogs < ActiveRecord::Migration
+  def change
+    add_column :blogs, :user_id, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+100.times do |n|
+    email = Faker::Internet.email
+    password = "password"
+    User.create!(email: email,
+                 password: password,
+                 password_confirmation: password
+    )
+end
+
+
+100.times do |n|
+    Blog.create(title:  Faker::Name.title,
+                content:   Faker::Hipster.sentence,
+                user_id: Faker::Number.between(1, 100)
+    )
+end
